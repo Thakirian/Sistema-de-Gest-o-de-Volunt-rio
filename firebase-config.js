f2adc8eece6c3290d6a00e60fea5aea44954986e
Thaisse Kirian <thakirian@gmail.com>
2025-05-07 21:25:55 -0300

    Create configuração firebase

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4iy8LfUcJLt7N62ZDXtZMEIio7jU9TNg",
+    authDomain: "gerenciamento-voluntario.firebaseapp.com",
+    projectId: "gerenciamento-voluntario",
+    storageBucket: "gerenciamento-voluntario.firebasestorage.app",
+    messagingSenderId: "1094500781796",
+    appId: "1:1094500781796:web:3fe0b16fbe4d51ecb526f1"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore();
+
+export { auth, db, createUserWithEmailAndPassword};
