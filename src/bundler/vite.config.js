04da3be09e4c31c60bb2b68b361965a3bcef2734
Thaisse Kirian <thakirian@gmail.com>
2025-06-19 17:22:03 -0300

    Create index

--- src/bundler/vite.config.js
+++ src/bundler/vite.config.js
@@ -5,18 +5,18 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  root: path.resolve(__dirname, '../'), // Aponte para a pasta src
+  root: path.resolve(__dirname, '../'), 
   publicDir: path.resolve(__dirname, '../public'),
   server: {
     port: 5173,
-    open: '/screans/cadastro/cadastro.html' // Abre automaticamente
+    open: 'http://localhost:5173/src/index.html',
   },
   build: {
     outDir: path.resolve(__dirname, '../../dist'),
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, '../screans/cadastro/cadastro.html')
+        main: path.resolve(__dirname, '../index.html'),
       }
     }
   }
